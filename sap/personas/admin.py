b6cff0bf45a04616a65041e99f6a6129508af32c
FRANCESC OLIVERAS <francescoli0218@gmail.com>
2022-11-06 18:02:48 +0100

    Object visualization changed

--- sap/personas/admin.py
+++ sap/personas/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from personas.models import Persona
 # Register your models here.
+admin.site.register(Persona) # Import personas to see in django admin
--- sap/personas/models.py
+++ sap/personas/models.py
@@ -6,3 +6,7 @@ class Persona(models.Model):
     name = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
+
+    # Change the visualization of the object in django admin
+    def __str__(self):
+        return f"Person {self.id}: Name: {self.name} {self.surname} {self.email}"
